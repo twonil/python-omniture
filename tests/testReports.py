c73f5e4732262b84fc349db89e1139a3fe51e092
Justin Grover <dancingcactus@gmail.com>
2016-07-21 23:57:46 -0600

    Big commit, Fixed the classification bug for real this time (thanks to @aarontoledo), Moved unittests to their own folder, setup a unittest with a mock object

--- tests/testReports.py
+++ tests/testReports.py
@@ -10,6 +10,8 @@ import pandas
 from pprint import pprint
 import inspect
 import datetime
+import requests_mock
+import json
 
 creds = {}
 creds['username'] = os.environ['OMNITURE_USERNAME']
@@ -20,17 +22,17 @@ test_report_suite = 'omniture.api-gateway'
 class ReportTest(unittest.TestCase):
     def setUp(self):
         self.analytics = omniture.authenticate(creds['username'], creds['secret'])
-        
+
     def tearDown(self):
         self.analytics = None
-        
+
     def test_basic_report(self):
         """ Make sure a basic report can be run
         """
         response = self.analytics.suites[test_report_suite].report.run()
-        
+
         self.assertIsInstance(response.data, list, "Something went wrong with the report")
-        
+
         #Timing Info
         self.assertIsInstance(response.timing['queue'], float, "waitSeconds info is missing")
         self.assertIsInstance(response.timing['execution'], float, "Execution info is missing")
@@ -55,12 +57,12 @@ class ReportTest(unittest.TestCase):
         self.assertIsInstance(response.data[0] , dict, "The data isn't getting into the dict")
         self.assertIsInstance(response.data[0]['datetime'], datetime.datetime, "The date isn't getting populated in the data")
         self.assertIsInstance(response.data[0]['pageviews'], int, "The pageviews aren't getting populated in the data")
-        
-            
+
+
     def test_ranked_report(self):
         """ Make sure the ranked report is being processed
         """
-        
+
         ranked = self.analytics.suites[test_report_suite].report.element("page").metric("pageviews").metric("visits")
         queue = []
         queue.append(ranked)
@@ -73,7 +75,7 @@ class ReportTest(unittest.TestCase):
             self.assertIsInstance(report.data[0]['page'], str, "The page isn't getting populated in the data")
             self.assertIsInstance(report.data[0]['pageviews'], int, "The pageviews aren't getting populated in the data")
             self.assertIsInstance(report.data[0]['visits'], int, "The visits aren't getting populated in the data")
-            
+
     def test_trended_report(self):
         """Make sure the trended reports are being processed corretly"""
         trended = self.analytics.suites[test_report_suite].report.element("page").metric("pageviews").granularity('hour').run()
@@ -82,17 +84,17 @@ class ReportTest(unittest.TestCase):
         self.assertIsInstance(trended.data[0]['datetime'], datetime.datetime, "The date isn't getting propulated correctly")
         self.assertIsInstance(trended.data[0]['page'], str, "The page isn't getting populated in the data")
         self.assertIsInstance(trended.data[0]['pageviews'], int, "The pageviews aren't getting populated in the data")
-        
+
     def test_dataframe(self):
         """Make sure the pandas data frame object can be generated"""
         trended = self.analytics.suites[test_report_suite].report.element("page").metric("pageviews").granularity('hour').run()
-        self.assertIsInstance(trended.dataframe, pandas.DataFrame, "Data Frame Object doesn't work")   
-        
+        self.assertIsInstance(trended.dataframe, pandas.DataFrame, "Data Frame Object doesn't work")
+
     def test_segments_id(self):
         """ Make sure segments can be added """
         suite = self.analytics.suites[test_report_suite]
         report = suite.report.filter(suite.segments[0]).run()
-        
+
         self.assertEqual(report.segments[0], suite.segments[0], "The segments don't match")
 
     @unittest.skip("skip inline segments because checked in Query")
@@ -101,7 +103,32 @@ class ReportTest(unittest.TestCase):
         #pretty poor check but need to make it work with any report suite
         report = self.analytics.suites[0].report.element('page').metric('pageviews').metric('visits').filter(element='browser', selected=["::unspecified::"]).run()
         self.assertIsInstance(report.data, list, "inline segments don't work")
-        
-    
+
+    @requests_mock.mock()
+    def test_multiple_classifications(self, m):
+        """Makes sure the report can parse multiple classifications correctly since they have the same element ID"""
+        #load sample file
+        with open('mock_objects/multi_classifications.json') as data_file:
+            json_response = data_file.read()
+
+        with open('mock_objects/Report.Queue.json') as queue_file:
+            ReportQueue = queue_file.read().replace('\n', '')
+
+        #setup mock object
+        m.post('https://api.omniture.com/admin/1.4/rest/?method=Company.GetReportSuites', text=json_response)
+        m.post('https://api.omniture.com/admin/1.4/rest/?method=Report.Get', text=json_response)
+        m.post('https://api.omniture.com/admin/1.4/rest/?method=Report.Queue', text=ReportQueue)
+
+        report = self.analytics.suites[0].report\
+            .element('evar2',classification="Classification 1", disable_validation=True)\
+            .element('evar2',classification="Classification 2", disable_validation=True)\
+            .run()
+
+
+        self.assertTrue('evar2 | Classification 1' in report.data[0])
+        self.assertTrue('evar2 | Classification 2' in report.data[0])
+
+
+
 if __name__ == '__main__':
     unittest.main()
